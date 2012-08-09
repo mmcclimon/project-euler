b9524cf6ac82ec8de51c419313e138be792dda1f
Michael McClimon <michael@mcclimon.org>
2012-08-09 10:40:35 -0400

    Another version for #24 using List::Permutor

--- p24.pl
+++ p24.pl
@@ -4,18 +4,23 @@ use strict;
 use v5.10;
 use Time::HiRes qw(gettimeofday tv_interval);
 
-use Algorithm::Loops qw(NextPermuteNum);
+use List::Permutor;
 
-# Euler Project #10
-# Find the sum of all the primes below two million.
+# Euler Project #24
+# What is the millionth lexicographic permutation of the digits 0, 1, 2, 3, 4, 5, 6, 7, 8 and 9?
 my $t0 = [gettimeofday()];
 
-my @list = 0..9;
-my @perms;
+my $perm = new List::Permutor (0..9);
 my $count = 0;
-do { $count++; push @perms, "@list" } while NextPermuteNum @list;
+my @perms;
+while (my @set = $perm->next) {
+	$count++;
+	last if $count > 1_000_000;
+	push @perms, "@set";
+}
 
 say "Total number of permutations: $count";
+#say for @perms;
 say "Millionth permutation: $perms[999_999]";
 
 say "\nFinished in ",tv_interval($t0, [gettimeofday()]) ," seconds.";
