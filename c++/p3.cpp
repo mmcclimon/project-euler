be41b7ca3251f694e7a5cd92b9428543863585f5
Michael McClimon <michael@mcclimon.org>
2012-09-09 17:10:23 -0400

    Solution for project 3, plus adding gitignore

---
+++ c++/p3.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <math.h>
+
+const long long TARGET = 600851475143LL;
+
+int main() {
+	using namespace std;
+
+	double root = sqrt(TARGET);
+	const unsigned int LIMIT = (int) root;
+	char sieve[LIMIT];
+	
+
+	// fill the sieve with 1s
+	for (unsigned int i=0; i < LIMIT; ++i)
+		sieve[i] = 1;
+
+
+	// after this loop, prime indexes have 1s
+	for (unsigned int i = 2; i < LIMIT; ++i) {
+		if (sieve[i] == 1) {
+			for (unsigned int j = i*i; j < LIMIT; j += i)
+				sieve[j] = 0;	
+		}
+	}
+
+	long long composite = TARGET;
+	unsigned int largestPrime = 0;
+
+
+	for (unsigned int i = 2; i < LIMIT; ++i) {
+		if (sieve[i] == 1) {
+			if (composite % i == 0) {
+				largestPrime = i;
+				composite /= i;
+				--i;  			// check this factor again
+			}
+		}
+	}
+
+	std::cout << "\nLargest prime: " << largestPrime << "\n";
+
+	return 0;
+}
